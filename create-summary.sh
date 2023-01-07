3ebcb9607951e2cc41e22ba2d1920a4d879be30b
Charlie Collard <charliecollard0@gmail.com>
2023-01-07 18:21:27 +0000

    Create summaries without GNU parallel

--- create-summary.sh
+++ create-summary.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 projectdir="/home/charlie/workspace/twitch-chat-replay"
-parallel $projectdir/summarize.sh ::: $projectdir/content/videos/*.json.gz | jq -s 'map(select(.created_at >= "2016-03-04T09:20:59Z")) | sort_by(.created_at) | reverse' | gzip - > $projectdir/content/vod-summaries.json.gz.tmp && mv $projectdir/content/vod-summaries.json.gz.tmp $projectdir/content/vod-summaries.json.gz
+find $projectdir/content/videos -name \*.json.gz -type f -print0 | xargs -P8 -0 -n1 $projectdir/summarize.sh | jq -s 'map(select(.created_at >= "2016-03-04T09:20:59Z")) | sort_by(.created_at) | reverse' | gzip - > $projectdir/content/vod-summaries.json.gz.tmp && mv $projectdir/content/vod-summaries.json.gz.tmp $projectdir/content/vod-summaries.json.gz
